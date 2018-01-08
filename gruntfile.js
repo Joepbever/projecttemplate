3c476abb167214071758be67fa887e8ad84cc213
Joepbever <32537516+Joepbever@users.noreply.github.com>
2018-01-08 13:57:47 +0100

    v3

--- gruntfile.js
+++ gruntfile.js
@@ -1,34 +1,26 @@
 module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
     grunt.initConfig({
-        jshint: {
-            files: ['gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-            options: {
-                reporterOutput: "",
-                globals: {
-                    jQuery: true
-                }
-            }
-        },
-        watch: {
-            files: ['<%= jshint.files %>'],
-            tasks: ['jshint']
+        eslint: {
+            target: ['./src/js/jscript.js']
+
+
         },
         cssmin: {
             target: {
                 files: [{
                     expand: true,
-                    cwd: 'css',
+                    cwd: 'src/css',
                     src: ['*.css', '!*.min.css'],
-                    dest: 'css',
-                    ext: '.min.css'
+                    dest: 'dist/css',
+                    ext: '.css'
                 }]
             }
         },
         uglify: {
             my_target: {
                 files: {
-                    'jscript.min.js': 'jscript.babel.js'
+                    'dist/js/jscript.js': 'jscript.babel.js'
                 }
             }
         },
@@ -39,15 +31,26 @@ module.exports = function(grunt) {
             },
             dist: {
                 files: {
-                    'jscript.babel.js': 'jscript.js'
+                    'jscript.babel.js': 'src/js/jscript.js'
                 }
+            }
+        },
+        copy: {
+            files: {
+                cwd: 'src/',  // set working folder / root to copy
+                src: 'index.html',           // copy all files and subfolders
+                dest: 'dist/',    // destination folder
+                expand: true
+
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('default', ['jshint','cssmin', 'babel', 'uglify' ]);
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.registerTask('default', ['eslint','cssmin', 'babel', 'uglify']);
+    grunt.registerTask('build', ['copy:html']);
 };
